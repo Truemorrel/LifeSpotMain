8f3a481deb714aa13e58f0808aae87d5d90848fc
Sergey Bednyakov <sbednyakov@gmail.com>
2022-11-14 06:43:35 +0300

    master; update sessionLog with window.sessionStorage

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -5,25 +5,44 @@
 * */
 //let session =  new Map();
 //let session = {};
-let session = {
-    'startDate': new Date().toLocaleString(),
-    'userAgent': window.navigator.userAgent,
-    'userAge': prompt("Пожалуйста, введите ваш возраст?")
-};
+//if (session.getItem('startDate')) {
+//    session.setItem('startDate', new Date().toLocaleString());
+//    session.setItem('userAgent', window.navigator.userAgent);
+//}
+//session.setItem('userAgent', window.navigator.userAgent);
+//if (session.getItem('userAge')) {
+//session.setItem('userAge', prompt("Пожалуйста, введите ваш возраст?"));
+//}
 
+let session = window.sessionStorage;
 /*
 * Сохранение данных сессии сразу при заходе пользователя на страницу
 * 
 * */
 
-function handleSession(){
+function handleSession(logger, checker) {
     // Сохраним время начала сессии
-    //session.set("startDate", new Date().toLocaleString())
-    session["startDate"] = new Date().toLocaleString();
+    //session["startDate"] = new Date().toLocaleString();
     // Сохраним UserAgent
     //session.set("userAgent", window.navigator.userAgent)
-    session["userAgent"] = window.navigator.userAgent;
-    session["userAge"] = prompt("Пожалуйста, введите ваш возраст?");
+    //session["userAgent"] = window.navigator.userAgent;
+    //session["userAge"] = prompt("Пожалуйста, введите ваш возраст?");
+
+    if (session.getItem('startDate') == null) {
+        session.setItem('startDate', new Date().toLocaleString());
+    };
+
+    if (session.getItem('userAgent') == null) {
+        session.setItem('userAgent', window.navigator.userAgent);
+    };
+    if (session.getItem('userAge') == null) {
+        
+        session.setItem('userAge', prompt('Введите свой возраст, пожалуйста:'));
+        checker(true);
+    } else {
+        checker(false);
+    };
+    logger();
 }
 
 /*
@@ -31,13 +50,16 @@ function handleSession(){
 * 
 * */
 
-function checkAge(){
-    //session["userAge"] = prompt("Пожалуйста, введите ваш возраст?")   
-    if (session["userAge"] >= 18){
-        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+let checker = function checkAge(newVisit) {
+    // session["userAge"] = prompt("Пожалуйста, введите ваш возраст?")
+    if (session.getItem('userAge') >= 18) {
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
-    else{
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
+    else {
+        session.setItem('userAgent', window.navigator.userAgent);
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
 }
@@ -52,12 +74,16 @@ function checkAge(){
 //        console.log(result)
 //    }
 //}
-
-let sessionLog = () => {
-    for (let item in session) {
-        console.log('\t' + item +':\t ' + session[item]);
-    }
+let logger = function () {
+    console.log('Начало сессии: ' + session.getItem("startDate"))
+    console.log('Даныне клиента: ' + session.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + session.getItem("userAge"))
 }
+//let logger = () => {
+//    for (let item in session) {
+//        console.log('\t' + item +':\t ' + session[item]);
+//    }
+//}
 
 /*
 * Функция для фильтраци контента
@@ -65,7 +91,7 @@ let sessionLog = () => {
 * 
 * */
 
-function filterContent(){
+function filterContent() {
     let elements = document.getElementsByClassName('video-container');
     
     for (let i = 0; i <= elements.length; i++ ){
