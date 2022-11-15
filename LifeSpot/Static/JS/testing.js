7cab9674f13a9279a561703ed1db8eb665ac34a7
Sergey Bednyakov <sbednyakov@gmail.com>
2022-11-15 05:55:36 +0300

    master: update

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -16,3 +16,39 @@ let newArrayOfStrings = [];
 // Позволяет на лету выполнять операции с парой ключ-значение
 Array.from(myMap, ([key,value]) => newArrayOfStrings.push(`${key} - ${value}`) );
 console.log(newArrayOfStrings);
+
+let input = window.sessionStorage;
+
+const listener = function () {
+    let prefix = 'arr';
+    let curIndex = 0;
+    let strOut = '';
+    let strIn = inputParseFunction();
+    if (input.getItem('curIndex') == null) {
+        input.setItem('curIndex', 1);
+        input.setItem(prefix + curIndex, strIn);
+        strOut = 'Ваш ввод: ' + strIn;
+    } else {
+        curIndex = Number(input.getItem('curIndex')); // 0
+        input.setItem(prefix + (curIndex + 1), strIn);
+        strOut = 'Ваш ввод: ' + strIn;
+        for (let i = curIndex; i > 0; i--) {
+            strOut += '\n' + input.getItem(prefix + (i - 1));
+        }
+        input.setItem('curIndex', ++curIndex)
+
+    }
+    alert(strOut);
+}
+
+//const listener = function () {
+//    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+//    let currentInput = document.getElementsByTagName('input')[0].value;
+
+//    // Покажем окно с прошлым и новым значением
+//    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+//        + '\n' + 'Текущий ввод: ' + currentInput);
+
+//    // Сохраним новое значение в контекст
+//    this.lastInput = currentInput;
+//}
