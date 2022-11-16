8d1b647e03c3658f2c9b6461c2a363b78952dcdc
Sergey Bednyakov <sbednyakov@gmail.com>
2022-11-16 03:04:19 +0300

    master: add likes

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -41,25 +41,35 @@ function getReview() {
 * Оставить комментарий
 *
 * */
-function getComment() {
+function Comment() {
     // Создаем объект обычного комментария
     let comment = {}
-
+    debugger
     // Запросим имя
-    comment.author = prompt("Как вас зовут ?")
-    if (comment.author == null) {
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
 
     // Запросим текст
-    comment.text = prompt("Оставьте отзыв")
-    if (comment.text == null) {
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
         return
     }
 
     // Сохраним текущее время
-    comment.date = new Date().toLocaleString()
+    this.date = new Date().toLocaleString()
+}
 
+let addComment = function () {
+    debugger
+    let comment = new Comment()
+
+    if (comment.empty) {
+        return;
+    }
     // Запросим,, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
     let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
 
@@ -77,6 +87,7 @@ function getComment() {
     }
 }
 
+
 /*
  
 /*
@@ -88,12 +99,40 @@ const writeReview = review => {
 
     // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
     if (review.hasOwnProperty('rate')) {
-        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+		// Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id ="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate} </button>`;
     }
 
     // Запишем результат
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
-        `<p>${review['text']}</p>` +
-        '</div>';
+        `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']} </p>` + '</div>';
+}
+
+/*
+* Увеличивает счётчик лайков
+*
+* */
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
 }
